8aa71f851c962bf772950a60bf0153bf3e3ede4d
Anat Benski Lipman <anat_benski@hotmail.com>
2018-07-25 17:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
-
+//document.querySelector('main').remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+function showWelcome() {
+    const newHeader = document.createElement('h1');
+    newHeader.id="victory";
+    newHeader.innerText="Anat the champion!";
+    document.body.appendChild(newHeader);
+    const newParagraph = document.createElement('p1');
+    newParagraph.innerText="wellcome";
+    document.body.appendChild(newParagraph);
+}
